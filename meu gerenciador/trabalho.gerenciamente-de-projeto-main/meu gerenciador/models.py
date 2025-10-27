3534bb622c5113f418e943766d8a005d6d7deff8
luanaburgareli <luana.burgarelip@gmail.com>
2025-10-27 11:17:48 -0300

    Add criar e validar (esboço)

---
+++ meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/models.py
@@ -0,0 +1,8 @@
+from storage import carregar_dados, salvar_dados
+from utils import gerar_id, validar_data, validar_email, diferenca_datas
+
+#def criar_usuario():
+    #validar
+    #salvar
+
+#bis, usuario, projetos, tarefas
