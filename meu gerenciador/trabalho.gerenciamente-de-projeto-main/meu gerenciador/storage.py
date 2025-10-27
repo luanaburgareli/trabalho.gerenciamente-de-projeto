452fd3375c78c9a838c8a52c1100387e55b4f5bf
luanaburgareli <luana.burgarelip@gmail.com>
2025-10-27 11:00:24 -0300

    Add funções json para armazenamento
    Implement functions to load and save JSON data for entities.

--- meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/storage.py
+++ meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/storage.py
@@ -1,1 +1,34 @@
-#(ler/gravar JSON) 
+import json
+import os
+
+data_dir = 'data'
+
+def caminho_arquivo(entity_name):
+    return os.path.join(data_dir, f'{entity_name}.json')
+
+def carregar_dados(entity_name):
+    caminho = caminho_arquivo(entity_name)
+    try:
+        with open(caminho, 'r', encoding='utf-8') as f:
+            conteudo = f.read()
+            if not conteudo:
+                return []
+            return json.loads(conteudo)
+    except (FileNotFoundError or json.JSONDecodeError):
+        return []
+    
+def salvar_dados(entity_name, data):
+    caminho = caminho_arquivo(entity_name)
+    try:
+        with open(caminho, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4, esfure_ascii=False)
+    except IOError as e:
+        print(f"Erro ao salvar dados para {entity_name}:{e}")
+        
+def iniciar_dados():
+    entities = ['usuarios','projetos','tarefas']
+    for entity in entities:
+        caminho = caminho_arquivo(entity)
+        if not os.path.exists(caminho):
+            salvar_dados(entity, [])
+    
