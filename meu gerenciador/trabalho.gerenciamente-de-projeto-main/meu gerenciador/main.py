90efbf7acbd743bcde363a697e937da2dc3081e9
luanaburgareli <luana.burgarelip@gmail.com>
2025-11-26 16:04:58 +0000

    corecação

--- meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/main.py
+++ meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/main.py
@@ -60,6 +60,8 @@ def menu_usuarios():
             if not validar_email(novo_email):
                 print("E-mail inválido.")
                 continue
+             if perfil not in ['usuario', 'admin']:
+                print("Perfil inválido.")
             success = atualizar_usuario(user_id, novo_nome, novo_email, novo_perfil)
             print("Usuário atualizado com sucesso." if success else "Erro ao atualizar usuário.")
         elif escolha == '5':
