05c4a7ae12bf15d97a8f68178687bb47af37656d
luanaburgareli <luana.burgarelip@gmail.com>
2025-11-27 11:35:37 -0300

    teste na prova

--- meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/utils.py
+++ meu gerenciador/trabalho.gerenciamente-de-projeto-main/meu gerenciador/utils.py
@@ -2,7 +2,7 @@
 import datetime
 import re
 import uuid
-from storage import carregar_dados
+ carregar_dados()
 
 def gerar_id(prefix: str) -> str:
     mapping = {'u': 'usuarios', 'p': 'projetos', 't': 'tarefas'}
